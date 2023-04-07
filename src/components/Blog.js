430da7078e461ecd3c636f2ba8ce280b3ec68730
johnegnatis <john@wavexchange.team>
2023-04-07 14:08:35 -0500

    css fix

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -9,7 +9,13 @@ import MainFeaturedPost from "./MainFeaturedPost";
 import FeaturedPost from "./FeaturedPost";
 import Main from "./Main";
 import Sidebar from "./Sidebar";
-import { blogs, mainBlog, featuredBlogs, getRoute, headerTitle } from "../blogs";
+import {
+  blogs,
+  mainBlog,
+  featuredBlogs,
+  getRoute,
+  headerTitle,
+} from "../blogs";
 
 const regularPosts = blogs.filter((_, index) => index > 2);
 
@@ -20,7 +26,7 @@ const sidebar = {
     "For us, hockey is more than just a game. It's a way for our family to come together and bond over a shared interest. We all share a deep passion for the sport, and our favorite NHL team is the Dallas Stars. On this blog, we'll be sharing our thoughts and insights about all things hockey, from the latest NHL news to tips for improving your game. So join us on this journey and be a part of the conversation.",
   ],
   social: [
-    { name: "GitHub", icon: GitHubIcon, url: 'https://github.com/johnegnatis' },
+    { name: "GitHub", icon: GitHubIcon, url: "https://github.com/johnegnatis" },
     { name: "Twitter", icon: TwitterIcon },
     { name: "Facebook", icon: FacebookIcon },
   ],
@@ -31,25 +37,22 @@ blogs.forEach((blog) => {
 
 export default function Blog() {
   return (
-    <>
-      <Header title={headerTitle} />
-      <main>
-        <MainFeaturedPost post={mainBlog} />
-        <Grid container spacing={4}>
-          {featuredBlogs.map((post) => (
-            <FeaturedPost key={post.title} post={post} />
-          ))}
-        </Grid>
-        <Grid container spacing={5} sx={{ mt: 3 }}>
-          <Main title="From the firehose" posts={regularPosts} />
-          <Sidebar
-            title={sidebar.title}
-            description={sidebar.description}
-            archives={sidebar.archives}
-            social={sidebar.social}
-          />
-        </Grid>
-      </main>
-    </>
+    <main>
+      <MainFeaturedPost post={mainBlog} />
+      <Grid container spacing={4}>
+        {featuredBlogs.map((post) => (
+          <FeaturedPost key={post.title} post={post} />
+        ))}
+      </Grid>
+      <Grid container spacing={5} sx={{ mt: 3 }}>
+        <Main title="From the firehose" posts={regularPosts} />
+        <Sidebar
+          title={sidebar.title}
+          description={sidebar.description}
+          archives={sidebar.archives}
+          social={sidebar.social}
+        />
+      </Grid>
+    </main>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Footer from "../components/Footer";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
-
+import Header from "../components/Header";
+import { headerTitle } from "../blogs";
 const theme = createTheme({
   palette: {
     mode: "dark",
@@ -13,6 +14,7 @@ export default function Home() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Header title={headerTitle} />
       <Container maxWidth="lg">
         <Blog />
       </Container>
